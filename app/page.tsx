f97927e823c4c7fffb3f8b62acac7b5466193e24
TourTask <info@tourtask.com>
2025-03-26 15:56:29 -0500

    Remove leaked sql credentials

--- app/page.tsx
+++ app/page.tsx
@@ -52,28 +52,3 @@ export default function Page() {
 		</main>
 	);
 }
-
-/*
-# Recommended for most uses
-DATABASE_URL=postgres://neondb_owner:npg_lQ59zcUBuZYf@ep-twilight-block-a6akylhw-pooler.us-west-2.aws.neon.tech/neondb?sslmode=require
-
-# For uses requiring a connection without pgbouncer
-DATABASE_URL_UNPOOLED=postgresql://neondb_owner:npg_lQ59zcUBuZYf@ep-twilight-block-a6akylhw.us-west-2.aws.neon.tech/neondb?sslmode=require
-
-# Parameters for constructing your own connection string
-PGHOST=ep-twilight-block-a6akylhw-pooler.us-west-2.aws.neon.tech
-PGHOST_UNPOOLED=ep-twilight-block-a6akylhw.us-west-2.aws.neon.tech
-PGUSER=neondb_owner
-PGDATABASE=neondb
-PGPASSWORD=npg_lQ59zcUBuZYf
-
-# Parameters for Vercel Postgres Templates
-POSTGRES_URL=postgres://neondb_owner:npg_lQ59zcUBuZYf@ep-twilight-block-a6akylhw-pooler.us-west-2.aws.neon.tech/neondb?sslmode=require
-POSTGRES_URL_NON_POOLING=postgres://neondb_owner:npg_lQ59zcUBuZYf@ep-twilight-block-a6akylhw.us-west-2.aws.neon.tech/neondb?sslmode=require
-POSTGRES_USER=neondb_owner
-POSTGRES_HOST=ep-twilight-block-a6akylhw-pooler.us-west-2.aws.neon.tech
-POSTGRES_PASSWORD=npg_lQ59zcUBuZYf
-POSTGRES_DATABASE=neondb
-POSTGRES_URL_NO_SSL=postgres://neondb_owner:npg_lQ59zcUBuZYf@ep-twilight-block-a6akylhw-pooler.us-west-2.aws.neon.tech/neondb
-POSTGRES_PRISMA_URL=postgres://neondb_owner:npg_lQ59zcUBuZYf@ep-twilight-block-a6akylhw-pooler.us-west-2.aws.neon.tech/neondb?connect_timeout=15&sslmode=require
-*/
